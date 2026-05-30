4e35974196fa2150b0cbe8800cc35002b27af507
Michael Mallo <21146674+michaelmallo@users.noreply.github.com>
2026-05-30 06:57:29 -0400

    Fixed cursor pointer on hover over multiple buttons

--- src/App.jsx
+++ src/App.jsx
@@ -335,7 +335,7 @@ export default function App() {
             <div className="flex bg-slate-100 p-1 rounded-lg w-full sm:w-max shadow-inner mt-1 md:mt-0 shrink-0">
               <button 
                 onClick={() => setViewMode('burnup')} 
-                className={`flex-1 sm:flex-none justify-center px-2 py-1 md:px-4 md:py-2 rounded-md text-xs md:text-sm font-bold transition-all duration-200 flex items-center gap-1 md:gap-2 cursor-pointer${viewMode === 'burnup' ? 'bg-emerald-600/75 shadow-sm text-white' : 'text-emerald-600 hover:text-emerald-700'}`}
+                className={`flex-1 sm:flex-none justify-center px-2 py-1 md:px-4 md:py-2 rounded-md text-xs md:text-sm font-bold transition-all duration-200 flex items-center gap-1 md:gap-2 cursor-pointer ${viewMode === 'burnup' ? 'bg-emerald-600/75 shadow-sm text-white' : 'text-emerald-600 hover:text-emerald-700'}`}
               >
                 <CheckCircle className={`w-3.5 h-3.5 md:w-4 md:h-4 ${viewMode === 'burnup' ? '' : 'text-emerald-600 hover:text-emerald-700'}`} />
                 Burn-up (Solved)
